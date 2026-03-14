8ddceb4c007917dcc158cc309ce4d972d48b13f5
Laysa-9E-ay2526 <laysa.dc@obmontessori.edu.ph>
2026-03-14 15:37:25 +0800

    Add fare calculation functions for regular and discounted fares

--- proj.js
+++ proj.js
@@ -1,1 +1,47 @@
+// Fare variables
+var basefare = 50;
+var perKMrate = 15;
+var baseKM = 2;
 
+// Regular fare calculation
+function regularFare(){
+
+var distance = document.getElementById("distance").value;
+
+var fare;
+
+if(distance <= baseKM){
+fare = basefare;
+}
+
+else{
+fare = basefare + ((distance - baseKM) * perKMrate);
+}
+
+document.getElementById("fare").innerHTML =
+"Fare: ₱" + fare;
+
+}
+
+// Discounted fare calculation
+function discountFare(){
+
+var distance = document.getElementById("distance").value;
+
+var fare;
+
+if(distance <= baseKM){
+fare = basefare;
+}
+
+else{
+fare = basefare + ((distance - baseKM) * perKMrate);
+}
+
+// apply 20% discount
+fare = fare * 0.80;
+
+document.getElementById("fare").innerHTML =
+"Fare: ₱" + fare;
+
+}
